9c49bbb6fc41866914d7367ae8a6af18ce644e0a
Joseph Priatel <joseph.priatel@gmail.com>
2025-02-16 14:59:00 -0800

    Added implementation of TypingTestHistory class

--- src/main/model/TypingTestHistory.java
+++ src/main/model/TypingTestHistory.java
@@ -1,7 +1,5 @@
 package model;
 
-import model.TypingTest;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,28 +10,48 @@ public class TypingTestHistory {
     // MODIFIES: this
     // EFFECTS: instantiates an empty typing test history
     public TypingTestHistory() {
+        history = new ArrayList<TypingTest>();
     }
 
     // MODIFIES: this
     // REQUIRES: test is not null
     // EFFECTS: adds a typing test to the history
     public void addTest(TypingTest test) {
+        history.add(test);
     }
 
     // EFFECTS: returns the history of typing tests
-    public ArrayList<TypingTest> getHistory() {
-        return null; //stub
+    public List<TypingTest> getHistory() {
+        return history;
     }
 
     // EFFECTS: returns the overall average WPM of all tests in history;
     //          if history is empty, returns 0
     public int getAverageWPM() {
-        return -1; //stub
+        if (history.isEmpty()) {
+            return 0;
+        }
+
+        int totalWPM = 0;
+        for (TypingTest test : history) {
+            totalWPM += test.getWPM();
+        }
+
+        return totalWPM / history.size();
     }
 
     // EFFECTS: returns the overall accuracy percentage of all tests in history;
     //          if history is empty, returns 0
     public int getAverageAccuracy() {
-        return -1; //stub
+        if (history.isEmpty()) {
+            return 0;
+        }
+
+        int totalAccuracy = 0;
+        for (TypingTest test : history) {
+            totalAccuracy += test.getAccuracy();
+        }
+
+        return totalAccuracy / history.size();
     }
 }
