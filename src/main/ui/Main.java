1b53ae45c54ce827749bd65613dd6a464bd15e2d
Joseph Priatel <joseph.priatel@gmail.com>
2025-03-02 15:17:10 -0800

    Updated Main class to catch FileNotFoundException

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,14 @@
 package ui;
 
+import java.io.FileNotFoundException;
+
 public class Main {
+    // runs console instance of typing test app
     public static void main(String[] args) throws Exception {
-        new TypingTestApp();
+        try {
+            new TypingTestApp();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to run application: file not found");
+        }
     }
 }
