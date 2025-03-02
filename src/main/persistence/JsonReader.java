19d1c6cc6e167126e9deef9f2cbb236c4c7c7859
Joseph Priatel <joseph.priatel@gmail.com>
2025-03-02 14:41:30 -0800

    Added implementation for JsonReader and JsonWriter classes

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -13,34 +13,59 @@ import org.json.*;
 
 // Represents a reader that reads workroom from JSON data stored in file
 public class JsonReader {
+    private String source;
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
+        this.source = source;
     }
 
     // EFFECTS: reads typing test history from file and returns it;
     // throws IOException if an error occurs reading data from file
     public TypingTestHistory read() throws IOException {
-        return null; //stub
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseHistory(jsonObject);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return null; //stub
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses typing test history from JSON object and returns it
     private TypingTestHistory parseHistory(JSONObject jsonObject) {
-        return null; //stub
+        TypingTestHistory history = new TypingTestHistory();
+        addTests(history, jsonObject);
+        return history;
     }
 
     // MODIFIES: history
     // EFFECTS: parses typing tests from JSON object and adds them to typing test history
     private void addTests(TypingTestHistory history, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("history");
+        for (Object json : jsonArray) {
+            JSONObject nextTest = (JSONObject) json;
+            addTest(history, nextTest);
+        }
     }
 
     // MODIFIES: history
     // EFFECTS: parses typing test from JSON object and adds it to typing test history
     private void addTest(TypingTestHistory history, JSONObject jsonObject) {
+        String difficulty = jsonObject.getString("difficulty");
+        int duration = jsonObject.getInt("duration");
+        String testContent = jsonObject.getString("testContent");
+        String userInput = jsonObject.getString("userInput");
+        int seed = jsonObject.getInt("seed");
+        TypingTest test = new TypingTest(difficulty, duration, testContent, userInput);
+        test.setSeed(seed);
+        history.addTest(test);
     }
 }
