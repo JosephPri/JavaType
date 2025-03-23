7aa16c9f01d11fc28beae3958749c110e2b34d05
Joseph Priatel <joseph.priatel@gmail.com>
2025-03-23 15:10:30 -0700

    Specified ResultsPopup class

---
+++ src/main/ui/popups/ResultsPopup.java
@@ -0,0 +1,17 @@
+package ui.popups;
+
+import javax.swing.*;
+
+import model.TypingTest;
+
+import java.awt.*;
+
+// represents a popup window that displays your test results
+public class ResultsPopup extends Popup {
+
+    // REQUIRES: test is not null
+    // EFFECTS: displays a pop up window with the results of given test
+    public ResultsPopup(TypingTest test) {
+        
+    }
+}
