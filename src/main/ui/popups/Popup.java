5f65717071d29d3d8cce29e6b30bb2da13fcfbe8
Joseph Priatel <joseph.priatel@gmail.com>
2025-03-23 15:09:15 -0700

    Implemented Popup class

--- src/main/ui/popups/Popup.java
+++ src/main/ui/popups/Popup.java
@@ -7,5 +7,9 @@ public abstract class Popup extends JDialog {
     
     //constructs popup window with no features
     public Popup(String title, int width, int height) {
+        setTitle(title);
+        setSize(width, height);
+        setLocationRelativeTo(null);
+        setModal(true);
     }
 }
