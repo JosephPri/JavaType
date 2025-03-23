5e5c64860d75c9a2dd5b29e8ce62241508ac08ce
Joseph Priatel <joseph.priatel@gmail.com>
2025-03-23 15:11:38 -0700

    Implemented ResultsPopup class

--- src/main/ui/popups/ResultsPopup.java
+++ src/main/ui/popups/ResultsPopup.java
@@ -12,6 +12,24 @@ public class ResultsPopup extends Popup {
     // REQUIRES: test is not null
     // EFFECTS: displays a pop up window with the results of given test
     public ResultsPopup(TypingTest test) {
-        
+        super("Test Results", 200, 150);
+        JPanel panel = new JPanel();
+
+        JLabel difficulty = new JLabel("Difficulty: " + test.getDifficulty());
+        JLabel duration = new JLabel("Duration: " + test.getDuration() + " seconds");
+        JLabel content = new JLabel("Content: " + test.getContentType());
+        JLabel seed = new JLabel("Seed: " + test.getSeed());
+        JLabel wpm = new JLabel("WPM: " + test.getWPM()); 
+        JLabel accuracy = new JLabel("Accuracy: " + test.getAccuracy() + "%");
+        panel.add(difficulty);
+        panel.add(duration);
+        panel.add(content);
+        if (test.getContentType().equals("random words")) {
+            panel.add(seed);
+        }
+        panel.add(wpm);
+        panel.add(accuracy);
+        add(panel);
+        setVisible(true);
     }
 }
