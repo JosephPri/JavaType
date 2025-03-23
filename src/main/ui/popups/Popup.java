e70face000444ff461b568b3a48f9ab6eb3380f5
Joseph Priatel <joseph.priatel@gmail.com>
2025-03-23 15:08:34 -0700

    Specified abstract class Popup

---
+++ src/main/ui/popups/Popup.java
@@ -0,0 +1,11 @@
+package ui.popups;
+
+import javax.swing.*;
+
+// represents an abstract popup window
+public abstract class Popup extends JDialog {
+    
+    //constructs popup window with no features
+    public Popup(String title, int width, int height) {
+    }
+}
