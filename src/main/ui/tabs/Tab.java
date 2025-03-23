9d66d25038dca3334fdbd144c3ff2ab0179449cd
Joseph Priatel <joseph.priatel@gmail.com>
2025-03-23 15:13:22 -0700

    Implemented Tab class

--- src/main/ui/tabs/Tab.java
+++ src/main/ui/tabs/Tab.java
@@ -12,17 +12,21 @@ public abstract class Tab extends JPanel {
 
     //REQUIRES: SwingTypingTestApp controller that holds this tab
     public Tab(SwingTypingTestApp controller) {
-        
+        this.controller = controller;
     }
 
     //EFFECTS: creates and returns row with button included
     public JPanel formatButtonRow(JButton b) {
-        
+        JPanel p = new JPanel();
+        p.setLayout(new FlowLayout());
+        p.add(b);
+
+        return p;
     }
 
     //EFFECTS: returns the SwingTypingTestApp controller for this tab
     public SwingTypingTestApp getController() {
-
+        return controller;
     }
 
 }
