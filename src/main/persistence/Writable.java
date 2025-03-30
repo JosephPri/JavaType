d06edbbde1bfdb02bec5228ed898c5209b62c599
Joseph Priatel <joseph.priatel@gmail.com>
2025-03-30 13:24:50 -0700

    Added class-level comment for Writable class

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,6 +2,7 @@ package persistence;
 
 import org.json.JSONObject;
 
+// represents a class that can be written as JSONObject
 public interface Writable {
     // EFFECTS: returns this as JSON object
     JSONObject toJson();
